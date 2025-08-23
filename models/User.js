7d2b3690dad15962151084628fae661aaefe63ab
ASUS <axmadov6greenclass@gmail.com>
2025-08-23 16:09:04 +0500

    12 player

--- models/User.js
+++ models/User.js
@@ -4,10 +4,12 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  point: {type: Number, default: 0, required: true},
-  gender: {type: String, required: true},
-  day: {type: Number, default: 0, required: true},
-  rank: {type: String, default: "E"}
+  point: {type: Number, default: 0},
+  firstReg: {type: String, default: "0/0/2000"},
+  LastSeen: {type: String, default: "0/0/2000"},
+  Play: {type: Number, default: 0},
+  Win: {type: Number, default: 0},
+  BeenSpy: {type: Number, default: 0}
 });
 
 export default mongoose.model('User', userSchema);
